7ca142ebaeebc676b5d989899f7b513264bb5c71
Philipp Winkler <philoc@freenet.de>
2020-12-20 18:23:20 +0100

    [FEATURE] adjust cms credentials

--- config/initializers/comfortable_mexican_sofa.rb
+++ config/initializers/comfortable_mexican_sofa.rb
@@ -90,8 +90,8 @@ end
 
 # Default credentials for ComfortableMexicanSofa::AccessControl::AdminAuthentication
 # YOU REALLY WANT TO CHANGE THIS BEFORE PUTTING YOUR SITE LIVE
-ComfortableMexicanSofa::AccessControl::AdminAuthentication.username = "username"
-ComfortableMexicanSofa::AccessControl::AdminAuthentication.password = "password"
+ComfortableMexicanSofa::AccessControl::AdminAuthentication.username = Rails.application.credentials.dig(:cms, :username) || "username"
+ComfortableMexicanSofa::AccessControl::AdminAuthentication.password = Rails.application.credentials.dig(:cms, :password) || "password"
 
 # Uncomment this module and `config.admin_auth` above to use custom admin authentication
 # module ComfyAdminAuthentication
